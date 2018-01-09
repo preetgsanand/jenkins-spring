398343e6b56eebef70f741dd5e1749d5faf0f4ea
preetgsa <preetanand90@gmail.com>
2018-01-09 21:55:17 +0530

    preetgsa - boilerplate spring app:

---
+++ src/main/java/com/jeet/jenkinsspring/JenkinsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.jeet.jenkinsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringApplication.class, args);
+	}
+}
